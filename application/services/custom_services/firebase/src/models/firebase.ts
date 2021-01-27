017c016dab94b6c4603e41e1b8ab458ef752c1a2
immanikandan <immanikandan>
2021-01-27 09:44:13 +0000

    updated generated code

--- application/services/custom_services/firebase/src/models/firebase.ts
+++ application/services/custom_services/firebase/src/models/firebase.ts
@@ -6,7 +6,9 @@ const Schema = mongoose.Schema;
 export const firebaseSchema = new Schema({
    firebase_url: String,
    firebase_name: String,
-   enter_name: String
+   enter_name: String,
+   test: String,
+   rock: String
 })
 
 const firebaseModel = mongoose.model('firebase', firebaseSchema, 'firebase');
